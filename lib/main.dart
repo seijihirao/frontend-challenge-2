df7c77c572f040daf2b66b6e632246b57fb6d886
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2021-12-05 22:45:52 -0300

    Feat: city screen part.
    Involves: #5

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:frontend_challenge_2/src/screens/list.dart';
 import 'package:frontend_challenge_2/src/screens/week.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(const DieselBankChallendApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class DieselBankChallendApp extends StatelessWidget {
+  const DieselBankChallendApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const ScreenWeatherList(),
-        '/week': (context) => const ScreenWeatherWeek(),
-      }
-    );
+        title: 'Diesel Bank Challenge',
+        theme: ThemeData(
+          primarySwatch: Colors.grey,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const ScreenWeatherList(),
+          '/week': (context) => const ScreenWeatherWeek(),
+        });
   }
 }
--- lib/src/screens/list.dart
+++ lib/src/screens/list.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+import 'package:frontend_challenge_2/src/apis/google_places.dart';
+import 'package:frontend_challenge_2/src/widgets/city_autocomplete.dart';
 
 class ScreenWeatherList extends StatelessWidget {
-  const ScreenWeatherList({ Key? key }) : super(key: key);
+  const ScreenWeatherList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Cities'),
+      ),
+      body: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TypeAheadField(
+                textFieldConfiguration: const TextFieldConfiguration(
+                  autofocus: true,
+                  decoration: InputDecoration(labelText: 'Search for a city'),
+                ),
+                suggestionsCallback: _search,
+                itemBuilder: (context, suggestion) {
+                  return ListTile(
+                    title: Text(suggestion as String),
+                  );
+                },
+                onSuggestionSelected: (suggestion) {
+                  print(suggestion);
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  Future<List<String?>> _search(String text) async {
+    var options = await GooglePlacesService.search(text);
+    List<String> optionsText = options['predictions']
+        .map((prediction) => prediction['description'])
+        .toList()
+        .cast<String>();
+    return optionsText;
   }
 }
