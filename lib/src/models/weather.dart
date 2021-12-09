817d64a96694f078af9dab6712aa0f75499fccaf
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2021-12-09 17:28:22 -0300

    Feat: openweather getWeek.

--- lib/src/models/weather.dart
+++ lib/src/models/weather.dart
@@ -2,10 +2,9 @@
 class Weather {
   final int id;
   final String city;
-  final String main;
-  final WeatherCondition condition;
+  List<WeatherCondition> conditions;
 
-  Weather(this.id, this.city, this.main, this.condition);
+  Weather(this.id, this.city, this.conditions);
 
   /// We only need to compare [id] on equals
   @override
@@ -15,11 +14,13 @@ class Weather {
 
   /// Create a hascode based on [id], [city] and [main] weather aspects
   @override
-  int get hashCode => Object.hash(id, city, main);
+  int get hashCode => Object.hash(id, city, conditions[0].description);
 }
 
 /// Weather condition object, containint [temperature], [pressure] and [humidity]
 class WeatherCondition {
+  final String description;
+  final String icon;
   final double temperature;
   final double feelsLike;
   final double temperatureMin;
@@ -27,6 +28,13 @@ class WeatherCondition {
   final int pressure;
   final int humidity;
 
-  WeatherCondition(this.temperature, this.feelsLike, this.temperatureMin,
-      this.temperatureMax, this.pressure, this.humidity);
+  WeatherCondition(
+      this.description,
+      this.icon,
+      this.temperature,
+      this.feelsLike,
+      this.temperatureMin,
+      this.temperatureMax,
+      this.pressure,
+      this.humidity);
 }
